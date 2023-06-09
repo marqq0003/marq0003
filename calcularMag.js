c052833ffca7759b675b1584015b1f53031907ed
marqq0003 <136092466+marqq0003@users.noreply.github.com>
2023-06-09 08:51:51 -0600

    Update calcularMag.js

--- calcularMag.js
+++ calcularMag.js
@@ -1,9 +1,1 @@
-function calcularMag ()
-{
-    var masa= 5.97E24;
-    var radio= 6378e3;
-    var volumen= (4/3)*Math.PI*radio*radio*radio;
-    var densidad= masa/volumen;
-    var d=document.getElementById("resultadoA");
-    d.innerHTML=densidad+' m/s';
-}
+
